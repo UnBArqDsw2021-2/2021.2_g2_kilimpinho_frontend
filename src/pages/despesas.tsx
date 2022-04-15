be73f9fdcb9531891b219a6fc6853b0355055824
luucas-melo <lucasmelodos322@gmail.com>
2022-04-15 15:23:57 -0300

    chore: Add CheckIsAdmin utils

--- src/pages/despesas.tsx
+++ src/pages/despesas.tsx
@@ -1,5 +1,10 @@
 import { ReactElement, useCallback, useState } from "react";
-import { GetStaticProps } from "next";
+import {
+  GetServerSideProps,
+  GetServerSidePropsContext,
+  GetServerSidePropsResult,
+  GetStaticProps,
+} from "next";
 import { BaseLayout } from "layouts/Base";
 import { NextPageWithLayout } from "@/types/next-page";
 import { ProfileForm } from "@/components/ProfileForm";
@@ -8,6 +13,24 @@ import { Divider } from "@/components/Divider";
 import { AdminLayout } from "layouts/Admin";
 import { ExpenditureForm } from "@/components/ExpenditureForm";
 import { Flex } from "@/UI/Flex";
+import { getSession } from "next-auth/react";
+import { CheckisAdmin } from "@/utils/isAdmin";
+
+export async function getServerSideProps(context: GetServerSidePropsContext) {
+  const isAdmin = CheckisAdmin(context);
+
+  if (!isAdmin) {
+    return {
+      redirect: {
+        destination: "/",
+        permanent: false,
+      },
+    };
+  }
+  return {
+    props: {}, // will be passed to the page component as props
+  };
+}
 
 const Auth: NextPageWithLayout = () => {
   const theme = useTheme();
